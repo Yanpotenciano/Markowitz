5503241bad58c738e729ba165a626b4e37ba3f46
Yanpotenciano <89522617+Yanpotenciano@users.noreply.github.com>
2022-05-20 09:52:41 -0300

    Update stdlib.R

--- stdlib.R
+++ stdlib.R
@@ -1,8 +1,14 @@
 #stdlib
-
+library(plotly)
+library(fPortfolio)
+library(BatchGetSymbols)
+# todas as funções que subscrevem tidyverse devem vir antes
 library(tidyverse)
 library(readxl)
 library(lubridate)
 library(mondate)
 library(gdata)
-library(purrr)
+library(lpSolve)
+library(lpSolveAPI)
+library(ggplot2)
+library(openxlsx)
