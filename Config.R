eb2425864114556f111067cee7a22d5ec5c1a467
Yanpotenciano <89522617+Yanpotenciano@users.noreply.github.com>
2022-07-15 14:51:27 -0300

    .

--- Config.R
+++
@@ -1,9 +0,0 @@
-require(lubridate)
-
-Ano_base = 2022
-Mes_base = 2 
-Meses_calculados = 13
-Data_base = dmy("01/02/2022")
-fund_register = "Fronteira_Markowitz_v14.xlsm"
-cvm_link = "http://dados.cvm.gov.br/dados/FI/DOC/INF_DIARIO/DADOS/inf_diario_fi_"
-cvm_link2 = "inf_diario_fi_"
